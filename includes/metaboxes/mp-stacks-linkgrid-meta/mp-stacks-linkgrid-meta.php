155a345ae7a19159164c81c30fb997335ab99fd9
Phil Johnston <support@mintplugins.com>
2015-09-20 14:22:30 -0400

    Brick Metabox controls now load using ajax.
    In MP Core 1.0.2.1, the metabox creation class allows for metabox contents to be loaded-in via ajax. The changes in this commit cause the controls for this metabox to use that functionality and will now load using ajax when chosen by the user as a Content-Type in MP Stacks.

--- includes/metaboxes/mp-stacks-linkgrid-meta/mp-stacks-linkgrid-meta.php
+++ includes/metaboxes/mp-stacks-linkgrid-meta/mp-stacks-linkgrid-meta.php
@@ -32,7 +32,8 @@ function mp_stacks_linkgrid_create_meta_box(){
 		'metabox_title' => __( '"LinkGrid" Content-Type', 'mp_stacks_linkgrid'), 
 		'metabox_posttype' => 'mp_brick', 
 		'metabox_context' => 'advanced', 
-		'metabox_priority' => 'low' 
+		'metabox_priority' => 'low',
+		'metabox_content_via_ajax' => true,
 	);
 	
 	/**
@@ -490,4 +491,5 @@ function mp_stacks_linkgrid_create_meta_box(){
 	global $mp_stacks_linkgrid_meta_box;
 	$mp_stacks_linkgrid_meta_box = new MP_CORE_Metabox($mp_stacks_linkgrid_add_meta_box, $mp_stacks_linkgrid_items_array);
 }
-add_action('mp_brick_metabox', 'mp_stacks_linkgrid_create_meta_box');
+add_action('mp_brick_ajax_metabox', 'mp_stacks_linkgrid_create_meta_box');
+add_action('wp_ajax_mp_stacks_linkgrid_metabox_content', 'mp_stacks_linkgrid_create_meta_box');
