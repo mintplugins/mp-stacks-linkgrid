1ce758383b28d91c9ac13cc05c4782f53083ce06
Phil Johnston <support@mintplugins.com>
2015-09-23 15:41:48 -0400

    Changed grid posts_per_row to use the "mp_stacks_grid_posts_per_row_percentage" function in MP Stacks.
    Changed grid posts_per_row to use the "mp_stacks_grid_posts_per_row_percentage" function in MP Stacks.

--- includes/misc-functions/content-filters-css.php
+++ includes/misc-functions/content-filters-css.php
@@ -65,7 +65,7 @@ function mp_stacks_brick_content_output_css_linkgrid( $css_output, $post_id, $fi
 	$css_output .= '
 	#mp-brick-' . $post_id . ' .mp-stacks-grid-item{' . 
 			mp_core_css_line( 'color', $default_text_color ) . 
-			mp_core_css_line( 'width', round((100/$linkgrid_per_row), 1), '%' ) . 
+			mp_core_css_line( 'width', mp_stacks_grid_posts_per_row_percentage( $linkgrid_per_row ), '%' ) . 
 			mp_core_css_line( 'padding', $linkgrid_post_spacing, 'px' ) . 
 	'}
 	#mp-brick-' . $post_id . ' .mp-stacks-grid-item-inner{' . 
